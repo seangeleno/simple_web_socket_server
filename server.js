9886634a5d05b728ade7b8220d9f08bbf4840d63
Sean Esteva <seangeleno@gmail.com>
2015-11-19 17:11:12 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app     = express()
+var http    = require('http')
+var httpServer = http.Server(app)
+var webSocketer = require('socket.io')(httpServer)
+
+app.get('/', function(request,response){
+ response.sendFile(__dirname + '/public/index.html')
+})
+
+webSocketer.on('connection', function(socket){
+ console.log('a user has connected')
+socket.on('color', function(data){
+ console.log(data.toUpperCase());
+});
+  socket.emit('server-message', 'hello from the server')
+});
+
+
+httpServer.listen(3000, function(){console.log('listening on 3000')})
